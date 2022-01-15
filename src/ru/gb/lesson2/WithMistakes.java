a254bc685a8c6966354c8cb8ed2f40f05f9a4970
Renat <ren_tirs@mail.ru>
2022-01-15 20:18:28 +0300

    Lesson1

---
+++ src/ru/gb/lesson2/WithMistakes.java
@@ -0,0 +1,56 @@
+package ru.gb.lesson2;
+
+public class WithMistakes {
+ /**   interface Moveable {
+        void move();
+    }
+    interface Stopable {
+        void stop();
+    }
+    abstract class Car {
+        public Engine engine; *Здесь нужен модификатор private
+        private String color;
+        private String name;
+        protected void start() { *Мне кажется, нужно сделать метод публичным public
+            System.out.println("Car starting");
+        }
+        abstract void open(); *Здесь тоже публичный public
+        public Engine getEngine() {
+            return engine;
+        }
+        public void setEngine(Engine engine) {
+            this.engine = engine;
+        }
+        public String getColor() {
+            return color;
+        }
+        public void setColor(String color) {
+            this.color = color;
+        }
+        public String getName() {
+            return name;
+        }
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+    class LightWeightCar extends Car implements Moveable{
+        @Override
+        void open() {
+            System.out.println("Car is open");
+        }
+        @Override
+        public void move() {
+            System.out.println("Car is moving");
+        }
+    }
+0 *здесь не нужен 0
+    class Lorry extends Car, Moveable, Stopable{ *должно быть Lorry extends Car, implements Moveable, Stopable
+        public void move(){ *нет аннотаций @Override, не переопределен метод open()
+            System.out.println("Car is moving");
+        }
+        public void stop(){
+            System.out.println("Car is stop");
+        }
+  **/
+}
---
+++ src/ru/gb/lesson3/Triangle.java
@@ -0,0 +1,8 @@
+package ru.gb.lesson3;
+
+public class Triangle extends Shape{
+    @Override
+    public void draw() {
+        System.out.println("Треугольник");
+    }
+}
